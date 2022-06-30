e56aa075f33b4add2759b7709ca1ac8470d47bf3
Matt11711 <matthew.d.allen97@gmail.com>
2022-06-30 18:30:25 -0400

    updated router structure

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { User, Thought, Reaction } = require("./models");
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes"));
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Notedb",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/challenge-18-db",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
